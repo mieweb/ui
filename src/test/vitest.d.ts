cd45750ee84cccef5e448b0792578c54727657f1
william garrity <wgarrity@mieweb.com>
2026-01-30 17:36:04 -0500

    style: Format vitest.d.ts with Prettier

--- src/test/vitest.d.ts
+++ src/test/vitest.d.ts
@@ -5,6 +5,8 @@ declare module 'vitest' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
   interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining
-    extends TestingLibraryMatchers<unknown, void> {}
+  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<
+    unknown,
+    void
+  > {}
 }
